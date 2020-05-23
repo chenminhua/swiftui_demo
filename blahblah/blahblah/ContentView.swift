7455c826575c2364295626e4a20de4853f9e63ba
陈敏华 <chenminhua@cn-yutdu-0702.fareast.corp.microsoft.com>
2020-05-23 09:53:51 +0800

    blahblah

---
+++ blahblah/blahblah/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  blahblah
+//
+//  Created by 陈敏华 on 2020/5/23.
+//  Copyright © 2020 陈敏华. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ blahblah/blahblahTests/blahblahTests.swift
@@ -0,0 +1,34 @@
+//
+//  blahblahTests.swift
+//  blahblahTests
+//
+//  Created by 陈敏华 on 2020/5/23.
+//  Copyright © 2020 陈敏华. All rights reserved.
+//
+
+import XCTest
+@testable import blahblah
+
+class blahblahTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
